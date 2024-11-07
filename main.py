5fadc7548ea726c5a8d7bacc15e65d853d9e2781
Marcus Holanda <mvsh777@hotmail.com>
2024-11-07 12:30:54 -0300

    doc export

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from ingestion_xml_gcs_athena.transform.raw import comand_raw
 from datetime import datetime
 import logging
 from datetime import datetime
+import argparse
+import sys
+from dateutil.relativedelta import relativedelta
 
 
 logging.basicConfig(
@@ -9,9 +12,52 @@ logging.basicConfig(
 )
 
 
+DEFAULT_END = datetime.now()
+DEFAULT_START = datetime.now().replace(day=1)
+
+if DEFAULT_END.day <= 5:
+    DEFAULT_START = DEFAULT_START - relativedelta(months=1) 
+
+
+parser = argparse.ArgumentParser(
+    prog='Export XML',
+    description='Extrai dados xml do sql server para Athena aws',
+    epilog="Des. Marcus holanda, System: %(prog)s"
+)
+
+parser.add_argument(
+    '-v', 
+    '--version', 
+    action='version'
+)
+parser.add_argument(
+    '-s',
+    '--start', 
+    help="Data inicial", 
+    type=lambda d: datetime.strptime(d, "%d/%m/%Y"),
+    default=DEFAULT_START
+)
+parser.add_argument(
+    '-e',
+    '--end',
+    help="Data final",
+    type=lambda d: datetime.strptime(d, "%d/%m/%Y"),
+    default=DEFAULT_END
+)
+
+
 if __name__ == '__main__':
-    comand_raw(
-        start=datetime(2024, 10, 1),
-        end=datetime(2024, 10, 31),
-        notes=['INCINERACAO', 'ESTORNO-INCINERACAO']
-    )
+    try:
+
+        args = parser.parse_args()
+    
+    except Exception:
+        parser.print_help()
+        sys.exit()
+
+    else:
+        comand_raw(
+            start=args.start,
+            end=args.end,
+            notes=['INCINERACAO', 'ESTORNO-INCINERACAO']
+        )
